adcf58f3b218ad2529486266fbd80d8b00eb84e4
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2023-01-26 01:58:35 +0800

    finish part3 3.19.-3.21.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -71,6 +71,12 @@ const App = () => {
           setTimeout(() => {
             setSuccessMessage(null);
           }, 5000);
+        })
+        .catch((error) => {
+          setErrorMessage(error.response.data.error);
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
         });
     }
     setNewName("");
