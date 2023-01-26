5db33477a2b61d4f55915c0a40b04e5a0b69f5a1
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2023-01-27 00:13:54 +0800

    finish part4 4.13.-4.14.

--- part4/bloglist/bloglist_backend/tests/blog_api.test.js
+++ part4/bloglist/bloglist_backend/tests/blog_api.test.js
@@ -4,6 +4,8 @@ const helper = require("./test_helper");
 const app = require("../app");
 const api = supertest(app);
 
+jest.setTimeout(100000);
+
 const Blog = require("../models/blog");
 
 beforeEach(async () => {
@@ -14,12 +16,12 @@ beforeEach(async () => {
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs");
   expect(response.body).toHaveLength(helper.initialBlogs.length);
-}, 100000);
+});
 
 test("the unique identifier property of the blog posts is named id", async () => {
   const blogs = await helper.blogsInDb();
   expect(blogs[0].id).toBeDefined();
-}, 100000);
+});
 
 test("a valid blog can be added", async () => {
   const newBlog = {
@@ -40,7 +42,7 @@ test("a valid blog can be added", async () => {
 
   const titles = blogsAtEnd.map((blog) => blog.title);
   expect(titles).toContain("Test Blog");
-}, 100000);
+});
 
 test("if the likes property is missing, it will default to the value 0", async () => {
   const newBlog = {
@@ -57,7 +59,7 @@ test("if the likes property is missing, it will default to the value 0", async (
 
   const blogsAtEnd = await helper.blogsInDb();
   expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0);
-}, 100000);
+});
 
 test("if the title and url properties are missing, the backend responds with the status code 400 Bad Request", async () => {
   const newBlog1 = {
@@ -74,7 +76,37 @@ test("if the title and url properties are missing, the backend responds with the
 
   await api.post("/api/blogs").send(newBlog1).expect(400);
   await api.post("/api/blogs").send(newBlog2).expect(400);
-}, 100000);
+});
+
+test("delete a blog", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+  const titles = blogsAtEnd.map((blog) => blog.title);
+  expect(titles).not.toContain(blogToDelete.title);
+});
+
+test("update a blog", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToUpdate = blogsAtStart[0];
+
+  const updatedBlog = {
+    title: blogToUpdate.title,
+    author: blogToUpdate.author,
+    url: blogToUpdate.url,
+    likes: blogToUpdate.likes + 1,
+  };
+
+  await api.put(`/api/blogs/${blogToUpdate.id}`).send(updatedBlog).expect(200);
+
+  const savedBlog = await Blog.findById(blogToUpdate.id);
+  expect(savedBlog.likes).toBe(updatedBlog.likes);
+});
 
 afterAll(async () => {
   await mongoose.connection.close();
