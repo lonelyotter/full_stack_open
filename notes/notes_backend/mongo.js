63147db4817b374b9078642c3bc2d60e944297c2
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2023-01-26 22:51:43 +0800

    update notes backend

--- notes/notes_backend/mongo.js
+++ notes/notes_backend/mongo.js
@@ -7,21 +7,33 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.ctlixc0.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullstack:${password}@cluster0.ctlixc0.mongodb.net/testNoteApp?retryWrites=true&w=majority`;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
 });
 
 const Note = mongoose.model("Note", noteSchema);
 
-Note.find({ important: true }).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
+const note = new Note({
+  content: "Who are you?",
+  date: new Date(),
+  important: false,
+});
+
+note.save().then(() => {
+  console.log("note saved!");
   mongoose.connection.close();
 });
+
+// Note.find({ important: true }).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
