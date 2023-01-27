6fc2b779e193358975ba8cd46c29191816d3a84d
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2023-01-27 20:11:11 +0800

    finish part4

--- part4/bloglist/bloglist_backend/controllers/blogs.js
+++ part4/bloglist/bloglist_backend/controllers/blogs.js
@@ -2,12 +2,13 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
+  const user = request.user;
 
   if (!body.title || !body.url) {
     return response.status(400).json({
@@ -20,15 +21,27 @@ blogsRouter.post("/", async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id,
   });
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id);
-  response.status(204).end();
+  const user = request.user;
+  const blog = await Blog.findById(request.params.id);
+  if (blog.user.toString() === user._id.toString()) {
+    user.blogs = user.blogs.filter((b) => b.toString() !== blog._id.toString());
+    await user.save();
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  } else {
+    response.status(401).json({ error: "blog does not exist" });
+  }
 });
 
 blogsRouter.put("/:id", async (request, response) => {
--- part4/bloglist/bloglist_backend/utils/middleware.js
+++ part4/bloglist/bloglist_backend/utils/middleware.js
@@ -1,10 +1,32 @@
 const logger = require("./logger");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
   logger.info("Path:  ", request.path);
   logger.info("Body:  ", request.body);
   logger.info("---");
+  next();
+};
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    request.token = authorization.replace("Bearer ", "");
+  }
+  next();
+};
+
+const userExtractor = async (request, response, next) => {
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token invalid" });
+    }
+    const user = await User.findById(decodedToken.id);
+    request.user = user;
+  }
   next();
 };
 
@@ -19,6 +41,14 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: "malformed id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({
+      error: "invalid token",
+    });
+  } else if (error.name === "TokenExpiredError") {
+    return response.status(401).json({
+      error: "token expired",
+    });
   }
 
   next(error);
@@ -26,6 +56,8 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   requestLogger,
+  tokenExtractor,
+  userExtractor,
   unknownEndpoint,
   errorHandler,
 };
