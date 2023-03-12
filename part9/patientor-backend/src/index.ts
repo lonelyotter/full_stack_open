dd117ac11bbe1cbd2be94e8a7499e3993d59b477
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2023-03-12 15:41:40 +0800

    finish part9 9.10.-9.11.

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import cors from "cors";
+
+import diagnoseRouter from "./routes/diagnoses";
+import patientRouter from "./routes/patients";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,6 +14,9 @@ app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnoseRouter);
+app.use("/api/patients", patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
