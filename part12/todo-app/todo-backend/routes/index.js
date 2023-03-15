3b786fb254569c03e08308279fb1716ed34ccc09
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2023-03-16 00:38:51 +0800

    finish part12 12.9.-12.11.

--- part12/todo-app/todo-backend/routes/index.js
+++ part12/todo-app/todo-backend/routes/index.js
@@ -1,17 +1,25 @@
-const express = require('express');
+const express = require("express");
+const redis = require("../redis");
 const router = express.Router();
 
-const configs = require('../util/config')
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
+  });
+});
+
+router.get("/statistics", async (req, res) => {
+  const added_todos = await redis.getAsync("added_todos");
+  res.send({
+    added_todos: parseInt(added_todos) || 0,
   });
 });
 
--- part12/todo-app/todo-backend/routes/todos.js
+++ part12/todo-app/todo-backend/routes/todos.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Todo } = require("../mongo");
+const redis = require("../redis");
 const router = express.Router();
 
 /* GET todos listing. */
@@ -14,6 +15,12 @@ router.post("/", async (req, res) => {
     text: req.body.text,
     done: false,
   });
+  const added_todos = await redis.getAsync("added_todos");
+  if (!parseInt(added_todos)) {
+    await redis.setAsync("added_todos", 1);
+  } else {
+    await redis.setAsync("added_todos", parseInt(added_todos) + 1);
+  }
   res.send(todo);
 });
 
