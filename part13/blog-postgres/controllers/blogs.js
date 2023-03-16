47931232cf823f2a5d48d103477291efa1ec9a91
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2023-03-16 15:02:35 +0800

    finish part13 13.7.

--- part13/blog-postgres/controllers/blogs.js
+++ part13/blog-postgres/controllers/blogs.js
@@ -7,12 +7,12 @@ router.get("/", async (req, res) => {
   res.json(blogs);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
   try {
     const blog = await Blog.create(req.body);
     res.json(blog);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    next(error);
   }
 });
 
@@ -28,13 +28,13 @@ router.delete("/:id", blogFinder, async (req, res) => {
   res.status(204).end();
 });
 
-router.put("/:id", blogFinder, async (req, res) => {
-  if (req.blog) {
+router.put("/:id", blogFinder, async (req, res, next) => {
+  try {
     req.blog.likes = req.body.likes;
     await req.blog.save();
     res.json(req.blog);
-  } else {
-    res.status(404).end();
+  } catch (error) {
+    next(error);
   }
 });
 
--- part13/blog-postgres/index.js
+++ part13/blog-postgres/index.js
@@ -10,6 +10,13 @@ app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  return res.status(400).json({ error: error.message });
+};
+
+app.use(errorHandler);
+
 const start = async () => {
   await connectToDatabase();
   app.listen(PORT, () => {
