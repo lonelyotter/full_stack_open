6a9ce11aae1e9ca7e06f95b0f96121a165beff9e
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2023-03-16 14:53:51 +0800

    finish part13 13.6.

--- part13/blog-postgres/controllers/blogs.js
+++ part13/blog-postgres/controllers/blogs.js
@@ -17,7 +17,7 @@ router.post("/", async (req, res) => {
 });
 
 const blogFinder = async (req, res, next) => {
-  req.blog = await Note.findByPk(req.params.id);
+  req.blog = await Blog.findByPk(req.params.id);
   next();
 };
 
@@ -26,6 +26,16 @@ router.delete("/:id", blogFinder, async (req, res) => {
     await req.blog.destroy();
   }
   res.status(204).end();
+});
+
+router.put("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes;
+    await req.blog.save();
+    res.json(req.blog);
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
